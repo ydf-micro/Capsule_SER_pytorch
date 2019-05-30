0567c0055df912a5388196f1fb01d691e54cedcb
ydf_micro <ydf_micro@163.com>
2019-05-30 16:04:58 +0800

    change the model and improve accuracy

--- src/CapsuleNet.py
+++ src/CapsuleNet.py
@@ -24,46 +24,6 @@ def softmax(input, dim=1):
     return softmaxed_output.view(*transposed_input.size()).transpose(dim, len(input.size()) - 1)
 
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, strides, padding):
-        super(ResidualBlock, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.strides = strides
-        self.padding = padding
-
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=self.in_channels,
-                      out_channels=self.out_channels,
-                      kernel_size=self.kernel_size,
-                      stride=self.strides,
-                      padding=self.padding),
-            nn.BatchNorm2d(self.out_channels),
-            nn.ReLU(),
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=self.in_channels,
-                      out_channels=self.out_channels,
-                      kernel_size=self.kernel_size,
-                      stride=self.strides,
-                      padding=self.padding),
-            nn.BatchNorm2d(self.out_channels),
-        )
-
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
 class BLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(BLSTM, self).__init__()
@@ -88,7 +48,7 @@ class BLSTM(nn.Module):
 class CapsuleLayer(nn.Module):
     def __init__(self, num_capsules,
                  in_channels, out_channels,
-                 kernel_size, strides):
+                 kernel_size, strides, padding):
         super(CapsuleLayer, self).__init__()
 
         self.num_capsules = num_capsules
@@ -97,10 +57,10 @@ class CapsuleLayer(nn.Module):
         self.kernel_size = kernel_size
         self.strides = strides
 
-        self.capsules = nn.ModuleList(   # 73*8*8*8
+        self.capsules = nn.ModuleList(   # 8*36*5*8
             [nn.Conv2d(self.in_channels, self.out_channels,
                        kernel_size=self.kernel_size,
-                       stride=self.strides, padding=0)
+                       stride=self.strides, padding=padding)
              for _ in range(self.num_capsules)]
         )
 
@@ -111,7 +71,7 @@ class CapsuleLayer(nn.Module):
         return scale * tensor / torch.sqrt(squared_norm + eps)
 
     def forward(self, x):
-        # [batch_size, 73*8*8, 1] * 8
+        # [batch_size, 8*36*5, 1] * 8
         outputs = [capsule(x).view(x.size(0), -1, 1) for capsule in self.capsules]
         outputs = torch.cat(outputs, dim=-1)
         outputs = self.squash(outputs)
@@ -144,9 +104,9 @@ class DigitCapsules(nn.Module):
 
     def forward(self, x):
         # None add 1 dimension
-        # x = [batch_size, 73*8*8, 8]
-        # self.rout_weights = [4, 73*8*8, 8, 16]
-        # priors: return [4, batch_size, 73*8*8, 1, 16]
+        # x = [batch_size, 8*38*5, 8]
+        # self.rout_weights = [4, 8*38*5, 8, 16]
+        # priors: return [4, batch_size, 8*38*5, 1, 16]
         priors = x[:, None, :, None, :] @ self.route_weights[None, :, :, :, :]
 
         logits = torch.zeros(priors.size()).cuda()
@@ -169,27 +129,26 @@ class CapsuleNet(nn.Module):
 
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=3,
-                      out_channels=64,
+                      out_channels=32,
                       kernel_size=(7, 1),
                       stride=1,
                       padding=(3, 0)),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=64,
-                      out_channels=128,
+            nn.Conv2d(in_channels=32,
+                      out_channels=64,
                       kernel_size=(3, 1),
                       stride=1,
                       padding=(1, 0)),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(64),
             nn.ReLU()
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=128,
+            nn.Conv2d(in_channels=64,
                       out_channels=128,
                       kernel_size=(1, 5),
                       stride=1,
@@ -200,52 +159,38 @@ class CapsuleNet(nn.Module):
 
         self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=128,
-                      out_channels=128,
+                      out_channels=256,
                       kernel_size=(1, 3),
                       stride=1,
                       padding=(0, 1)),
-            nn.BatchNorm2d(128),
-            nn.ReLU()
-        )
-
-        self.redidualBlock1 = ResidualBlock(
-            in_channels=128,
-            out_channels=128,
-            kernel_size=(1, 5),
-            strides=1,
-            padding=(0, 2)
-        )
-
-        self.redidualBlock2 = ResidualBlock(
-            in_channels=128,
-            out_channels=128,
-            kernel_size=(1, 3),
-            strides=1,
-            padding=(0, 1)
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         )
 
         self.blstm = BLSTM(
-            input_size=20*128,
+            input_size=20 * 128,
             hidden_size=128,
-            num_layers=3,
+            num_layers=2,
             num_classes=4
         )
 
         self.primaryCapsules = CapsuleLayer(
             num_capsules=8,
-            in_channels=128,
+            in_channels=256,
             out_channels=8,
-            kernel_size=5,
-            strides=2
+            kernel_size=(5, 3),
+            strides=2,
+            padding=(2, 1)
         )
 
         self.digitCapsules = DigitCapsules(
             num_capsules=NUM_CLASS,
-            num_route_nodes=73*8*8,
+            num_route_nodes=8*38*5,
             in_channels=8,
             out_channels=16
         )
-
 
     def forward(self, x):
         x = self.conv1(x)
@@ -254,18 +199,21 @@ class CapsuleNet(nn.Module):
         x = self.conv4(x)
         to_lstm = x.permute(0, 2, 1, 3)
         to_lstm = to_lstm.contiguous().view(-1, to_lstm.size(1),
-                                            to_lstm.size(2)*to_lstm.size(3))
+                                            to_lstm.size(2) * to_lstm.size(3))
         to_lstm = self.blstm(to_lstm)
 
-        # to_caps = self.primaryCapsules(x)
-        # to_caps = self.digitCapsules(to_caps)
-        #
-        # to_caps = torch.sqrt(torch.sum(torch.pow(to_caps, 2), dim=-1))
-        #
-        # output = to_lstm + to_caps
-        #
-        # return output
-        return to_lstm
+        to_caps = self.primaryCapsules(x)
+        to_caps = self.digitCapsules(to_caps)
+
+        to_caps = torch.sqrt(torch.sum(torch.pow(to_caps, 2), dim=-1))
+
+        # print(to_lstm)
+        # print(to_caps)
+
+        out = to_lstm + to_caps
+        # print(out)
+
+        return out
 
 
 def load_data(in_dir):
@@ -305,9 +253,12 @@ def load_data(in_dir):
     return train_loader, valid_loader, test_loader
 
 
-def train(capsNet, optimizer, loss_func, train_loader, valid_loader):
+def train(capsNet, optimizer, scheduler, loss_func, train_loader, valid_loader):
 
     for epoch in range(1, EPOCHS + 1):
+        train_loss = 0.
+        train_result = []
+        train_label = []
         for step, (batch_x, batch_y) in enumerate(train_loader):
             batch_x = batch_x.cuda()
             batch_y = batch_y.cuda()
@@ -320,91 +271,84 @@ def train(capsNet, optimizer, loss_func, train_loader, valid_loader):
 
             pred = torch.argmax(pred, dim=1)
 
-            train_accuracy = torch.sum(pred == batch_y).type(torch.FloatTensor) / batch_y.size(0)
-            print('Epoch: {:0>3}/{}'.format(epoch, EPOCHS),
-                  '| step: {:0>2}/{}'.format(step+1, len(train_loader)),
-                  '| loss: {:.2f}'.format(loss.detach().cpu().numpy()),
-                  '| train accuracy: {:.2%}'.format(train_accuracy.detach().cpu().numpy()))
+            train_result.extend(pred.cpu())
+            train_label.extend(batch_y.cpu())
+            train_loss += float(loss)
+
+            train_accuracy = (pred==batch_y).float().mean()
+
+            print(f'Epoch: {epoch:0>3}/{EPOCHS}',
+                  f'| step: {step+1:0>2}/{len(train_loader)}',
+                  f'| loss: {loss.item():.2f}',
+                  f'| train accuracy: {train_accuracy.item():.2%}')
 
             if step == len(train_loader) - 1:
 
-                capsNet.eval()
+                train_result = torch.tensor(train_result)
+                train_label = torch.tensor(train_label)
 
-                with torch.no_grad():
+                train_weighted_accuracy = weighted_accuracy(train_result, train_label)
+                train_unweighted_accuracy = unweighted_accuracy(train_result, train_label)
+                train_confusion = confusion(train_label, train_result,
+                                            labels=list(range(4)))
 
-                    train_loss = 0.
-                    train_result = []
-                    train_label = []
+                train_loss /= len(train_loader)
+
+                print(f'------------Epoch: {epoch:0>3}/{EPOCHS}',
+                      f'| train loss: {train_loss:.2f}',
+                      f'| train weighted accuracy: {train_weighted_accuracy.item():.2%}',
+                      f'| train unweighted accuracy: {train_unweighted_accuracy.item():.2%}',
+                      '\ntrain confusion matrix:[hap, sad, ang, neu]\n', train_confusion)
+
+            torch.cuda.empty_cache()
 
 
-                    for (batch_train_x, batch_train_y) in train_loader:
-                        batch_train_x = batch_train_x.cuda()
-                        batch_train_y = batch_train_y.cuda()
+        if epoch % 10 == 0:
+            torch.save(capsNet, '../model/model_{:0>2}.pkl'.format(epoch))
+            print('save the intermediate model successfully')
 
-                        train_pred = capsNet(batch_train_x)
-                        b_train_loss = loss_func(train_pred, batch_train_y)
-                        train_loss += float(b_train_loss)
+            capsNet.eval()
 
-                        train_pred = torch.argmax(train_pred, dim=1)
-                        train_result.extend(train_pred.detach().cpu().numpy())
-                        train_label.extend(batch_train_y.detach().cpu().numpy())
+            with torch.no_grad():
 
-                    train_weighted_accuracy = weighted_accuracy(train_result, train_label)
-                    train_unweighted_accuracy = unweighted_accuracy(train_result, train_label)
-                    train_confusion = confusion(train_label, train_result,
-                                                labels=list(range(4)))
+                valid_loss = 0.
+                valid_result = []
+                valid_label = []
 
-                    train_loss /= len(train_loader)
+                for (batch_valid_x, batch_valid_y) in valid_loader:
+                    batch_valid_x = batch_valid_x.cuda()
+                    batch_valid_y = batch_valid_y.cuda()
 
-                    print('------------Epoch: {:0>3}/{}'.format(epoch, EPOCHS),
-                          '| train loss: {:.2f}'.format(train_loss),
-                          '| train weighted accuracy: {:.2%}'.format(train_weighted_accuracy),
-                          '| train unweighted accuracy: {:.2%}'.format(train_unweighted_accuracy),
-                          '\ntrain confusion matrix:\n', train_confusion)
+                    valid_pred = capsNet(batch_valid_x)
+                    b_valid_loss = loss_func(valid_pred, batch_valid_y)
+                    valid_loss += float(b_valid_loss)
 
-                capsNet.train()
+                    valid_pred = torch.argmax(valid_pred, dim=1)
 
-            if epoch % 10 == 0 and step == len(train_loader) - 1:
-                torch.save(capsNet, '../model/model_{:0>2}.pkl'.format(epoch))
-                print('save the intermediate model successfully')
+                    valid_result.extend(valid_pred.cpu())
+                    valid_label.extend(batch_valid_y.cpu())
 
-                capsNet.eval()
+                valid_result = torch.tensor(valid_result)
+                valid_label = torch.tensor(valid_label)
 
-                with torch.no_grad():
+                valid_weighted_accuracy = weighted_accuracy(valid_result, valid_label)
+                valid_unweighted_accuracy = unweighted_accuracy(valid_result, valid_label)
+                valid_confusion = confusion(valid_label, valid_result,
+                                            labels=list(range(4)))
 
-                    valid_loss = 0.
-                    valid_result = []
-                    valid_label = []
+                valid_loss /= len(valid_loader)
 
-                    for (batch_valid_x, batch_valid_y) in valid_loader:
-                        batch_valid_x = batch_valid_x.cuda()
-                        batch_valid_y = batch_valid_y.cuda()
+                print(f'------------Epoch: {epoch:0>3}',
+                      f'| cross_validation loss: {valid_loss:.2f}',
+                      f'| cross_validation weighted accuracy: {valid_weighted_accuracy.item():.2%}',
+                      f'| cross_validation unweighted accuracy: {valid_unweighted_accuracy.item():.2%}',
+                      '\nvalid confusion matrix:[hap, sad, ang, neu]\n', valid_confusion)
 
-                        valid_pred = capsNet(batch_valid_x)
-                        b_valid_loss = loss_func(valid_pred, batch_valid_y)
-                        valid_loss += float(b_valid_loss)
+            capsNet.train()
 
-                        valid_pred = torch.argmax(valid_pred, dim=1)
+            torch.cuda.empty_cache()
 
-                        valid_result.extend(valid_pred.detach().cpu().numpy())
-                        valid_label.extend(batch_valid_y.detach().cpu().numpy())
-
-                    valid_weighted_accuracy = weighted_accuracy(valid_result, valid_label)
-                    valid_unweighted_accuracy = unweighted_accuracy(valid_result, valid_label)
-                    valid_confusion = confusion(valid_label, valid_result,
-                                                labels=list(range(4)))
-
-                    valid_loss /= len(valid_loader)
-
-                    print('------------Epoch: {:0>3}'.format(epoch),
-                          '| cross_validation loss: {:.2f}'.format(valid_loss),
-                          '| cross_validation weighted accuracy: {:.2%}'.format(valid_weighted_accuracy),
-                          '| cross_validation unweighted accuracy: {:.2%}'.format(valid_unweighted_accuracy),
-                          '\nvalid confusion matrix:\n', valid_confusion)
-
-                capsNet.train()
-
-        torch.cuda.empty_cache()
+        scheduler.step()
 
 
 def test(capsNet, loss_func, test_loader):
@@ -423,8 +367,11 @@ def test(capsNet, loss_func, test_loader):
 
             test_pred = torch.argmax(test_pred, dim=1)
 
-            test_result.extend(test_pred.detach().cpu().numpy())
-            test_label.extend(batch_test_y.detach().cpu().numpy())
+            test_result.extend(test_pred.cpu())
+            test_label.extend(batch_test_y.cpu())
+
+        test_result = torch.tensor(test_result)
+        test_label = torch.tensor(test_label)
 
         test_weighted_accuracy = weighted_accuracy(test_result, test_label)
         test_unweighted_accuracy = unweighted_accuracy(test_result, test_label)
@@ -433,43 +380,59 @@ def test(capsNet, loss_func, test_loader):
 
         test_loss /= len(test_loader)
 
-        print('------------test loss: {:.2f}'.format(test_loss),
-              '| test weighted accuracy: {:.2%}'.format(test_weighted_accuracy),
-              '| test unweighted accuracy: {:.2%}'.format(test_unweighted_accuracy),
-              '\ntest confusion matrix:\n', test_confusion)
+        print(f'------------test loss: {test_loss:.2f}',
+              f'| test weighted accuracy: {test_weighted_accuracy.item():.2%}',
+              f'| test unweighted accuracy: {test_unweighted_accuracy.item():.2%}',
+              '\ntest confusion matrix:[hap, sad, ang, neu]\n', test_confusion)
 
 
 def unweighted_accuracy(y_true, y_pred):
-    y_true = np.array(y_true)
-    y_pred = np.array(y_pred)
-    classes = np.unique(y_true)
-    classes_accuracies = np.zeros(classes.shape[0])
+    classes = torch.unique(y_true)
+    classes_accuracies = torch.zeros(classes.shape[0])
     for num, cls in enumerate(classes):
         classes_accuracies = weighted_accuracy(y_true[y_true == cls], y_pred[y_true == cls])
 
-    return np.mean(classes_accuracies)
+    return classes_accuracies.mean()
 
 
 def weighted_accuracy(y_true, y_pred):
-    return np.sum(np.array(y_true) == np.array(y_pred)) / len(y_true)
+
+    return (y_true == y_pred).float().mean()
 
 
 if __name__ == '__main__':
     start = time.time()
     capsNet = CapsuleNet()
+    print(capsNet)
     capsNet.cuda()
     summary(capsNet, (3, 300, 40))
 
     print('# parameters:', sum(param.numel() for param in capsNet.parameters()))
 
+    # # Use L2 normalization
+    # weight_p, bias_p = [], []
+    # for name, p in capsNet.named_parameters():
+    #     if 'bias' in name:
+    #         bias_p += [p]
+    #     else:
+    #         weight_p += [p]
+    #
+    #
+    # optimizer = torch.optim.Adam([
+    #     {'params': weight_p, 'weight_decay': 0.1},
+    #     {'params': bias_p, 'weight_decay': 0}
+    #     ], lr=LR)
+
     optimizer = torch.optim.Adam(capsNet.parameters(), lr=LR)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
+
     loss_func = nn.CrossEntropyLoss()
 
     train_loader, valid_loader, test_loader = load_data('../data/impro_IEMOCAP.pkl')
 
-    train(capsNet, optimizer, loss_func, train_loader, valid_loader)
+    train(capsNet, optimizer, scheduler, loss_func, train_loader, valid_loader)
 
     test(capsNet, loss_func, test_loader)
 
     end = time.time()
-    print('总用时：{:.2f}mins'.format((end - start) / 60))
+    print(f'总用时：{(end - start) / 60:.2f}mins')
